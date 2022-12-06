1a08fd7ba69d675d0d1f01a33aae51b89d7ace3b
Pavel Pratyush <akpratyush@gmail.com>
2022-12-06 09:28:33 +0100

    Part 2

--- day-6/main.go
+++ day-6/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func checkUnique(array [4]string) bool {
+func checkUnique(array [14]string) bool {
 	for k, v := range array {
 		for _, w := range array[k+1:] {
 			if v == w {
@@ -30,20 +30,37 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	for fileScanner.Scan() {
-		var bufferArray [4]string
+		// var bufferArray [4]string
+		// stringVal := fileScanner.Text()
+		// //fmt.Println("File Line", stringVal)
+		// for i := 3; i < len(stringVal); i++ {
+		// 	bufferArray[0] = string(stringVal[i-3])
+		// 	bufferArray[1] = string(stringVal[i-2])
+		// 	bufferArray[2] = string(stringVal[i-1])
+		// 	bufferArray[3] = string(stringVal[i])
+		// 	//fmt.Println(bufferArray)
+		// 	if checkUnique(bufferArray) {
+		// 		fmt.Println("Uniqe key is ", i+1)
+		// 		break
+		// 	}
+		// }
+
+		var bufferArray [14]string
 		stringVal := fileScanner.Text()
 		//fmt.Println("File Line", stringVal)
-		for i := 3; i < len(stringVal); i++ {
-			bufferArray[0] = string(stringVal[i-3])
-			bufferArray[1] = string(stringVal[i-2])
-			bufferArray[2] = string(stringVal[i-1])
-			bufferArray[3] = string(stringVal[i])
-			fmt.Println(bufferArray)
+		for i := 13; i < len(stringVal); i++ {
+			j := 0
+			for k := i - 13; k <= i; k++ {
+				bufferArray[j] = string(stringVal[k])
+				j++
+			}
+			//fmt.Println(bufferArray)
 			if checkUnique(bufferArray) {
 				fmt.Println("Uniqe key is ", i+1)
 				break
 			}
 		}
+
 	}
 
 	readFile.Close()
