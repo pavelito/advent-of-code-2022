c98c9deb7b273f24f9d928658d2a8b11b7f38396
Pavel Pratyush <akpratyush@gmail.com>
2022-12-13 16:32:26 +0100

    Part 1

---
+++ day-13/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+)
+
+func main() {
+	input, _ := os.ReadFile("input.txt")
+
+	messages := [][]any{}
+	for _, line := range strings.Split(string(input), "\n") {
+		if line == "" {
+			continue
+		}
+
+		var packet []any
+		json.Unmarshal([]byte(line), &packet)
+		messages = append(messages, packet)
+
+	}
+
+	// Part 1
+	sumOfIndexes := 0
+	for i := 0; i < len(messages); i += 2 {
+		if isLess(messages[i], messages[i+1]) {
+			sumOfIndexes += (i / 2) + 1
+		}
+	}
+
+	fmt.Println("Part 1: ", sumOfIndexes)
+}
+
+func isLess(message1 []any, message2 []any) bool {
+	return compare(message1, message2) < 1
+}
+
+func compare(message1 []any, message2 []any) int {
+	for i := 0; i < len(message1) && i < len(message2); i++ {
+		l := message1[i]
+		r := message2[i]
+
+		lIsNum := reflect.TypeOf(l).Name() == "float64"
+		rIsNum := reflect.TypeOf(r).Name() == "float64"
+
+		if lIsNum && rIsNum {
+			if l.(float64) < r.(float64) {
+				return -1
+			}
+			if l.(float64) > r.(float64) {
+				return 1
+			}
+		} else {
+			var lChildren []any
+			var rChildren []any
+
+			if lIsNum {
+				lChildren = []any{l}
+			} else {
+				lChildren = l.([]any)
+			}
+
+			if rIsNum {
+				rChildren = []any{r}
+			} else {
+				rChildren = r.([]any)
+			}
+
+			res := compare(lChildren, rChildren)
+
+			if res != 0 {
+				return res
+			}
+		}
+	}
+
+	if len(message1) < len(message2) {
+		return -1
+	}
+
+	if len(message1) > len(message2) {
+		return 1
+	}
+
+	return 0
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module pavelito.github.com/advent-of-code
 go 1.18
 
 require github.com/gammazero/deque v0.2.1
+
+require (
+	github.com/benpate/convert v0.13.5 // indirect
+	github.com/benpate/derp v0.22.2 // indirect
+	github.com/benpate/null v0.6.4 // indirect
+)
